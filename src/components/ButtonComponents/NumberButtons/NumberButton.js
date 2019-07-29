ed4f43dc54fffd19dc7f034e2d10ea2f18ed94ba
Logan Karnes <logan.karnes98@gmail.com>
2019-07-28 20:07:43 -0700

    added the zero id so its bigger... fully completed

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -4,7 +4,7 @@ import "../../../App";
 const NumberButton = props => {
   const int = props.int;
   return (
-    <button className='number'>
+    <button className='number' id={'a'+int}>
       {int/* Display a button element rendering the data being passed down from the parent container on props */}
     </button>
   );
